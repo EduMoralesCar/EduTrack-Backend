029c45404a6fad333ef18209424d16e7769e8687
Edomocar <edumoralescarlos@gmail.com>
2026-05-21 10:48:22 -0500

    feat(usuarios): definir mapeo de entidad JPA User e interfaz UserRepository

---
+++ src/main/java/com/utp/EduTrack/persistance/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.utp.EduTrack.persistance.repository;
+
+import com.utp.EduTrack.persistance.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsernameAndIdNot(String username, Long id);
+    boolean existsByEmailAndIdNot(String email, Long id);
+}
