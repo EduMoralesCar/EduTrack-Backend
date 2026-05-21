72ae822cb93bb7ffeb470c75eaccf78440128f7f
Edomocar <edumoralescarlos@gmail.com>
2026-05-21 11:06:42 -0500

    feat(matriculas): establecer mapeos JPA y repositorios para secciones y matriculas

---
+++ src/main/java/com/utp/EduTrack/persistance/repository/EnrollmentRepository.java
@@ -0,0 +1,12 @@
+package com.utp.EduTrack.persistance.repository;
+
+import com.utp.EduTrack.persistance.entity.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    List<Enrollment> findBySectionId(Long sectionId);
+    List<Enrollment> findByStudentId(Long studentId);
+    boolean existsByStudentIdAndSectionId(Long studentId, Long sectionId);
+}
